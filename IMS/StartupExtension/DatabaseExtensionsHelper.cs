cd9d97a9b8db6311d33764f47d3039ed3432397b
Md Faizul Haque <csldotnet@gmail.com>
2026-03-20 21:08:33 +0600

    Add project files.

---
+++ IMS/StartupExtension/DatabaseExtensionsHelper.cs
@@ -0,0 +1,38 @@
+﻿using IMS.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMS.StartupExtension
+{
+    public static class DatabaseExtensionsHelper
+    {
+        public static IServiceCollection AddDatabaseExtensionHelper(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<ApplicationDbContext>(Opt =>
+            {
+
+                Opt.UseSqlServer(configuration.GetConnectionString(name: "DefaultConnection"));
+
+            });
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder RunMigration(this IApplicationBuilder app)
+        {
+
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                db.Database.Migrate();
+            }
+
+            return app;
+        }
+    }
+}
